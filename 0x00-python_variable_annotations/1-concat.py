58fc4f703ae15282eb44326690af74ec3bb0df75
azziimm7 <azzah31@outlook.com>
2024-08-01 17:35:48 +0000

    concatentae

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A module that defines a function that concatente 2 strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate 2 strings"""
+    return str1 + str2
