db161123984b6c4ffa5691dcf1af296ebea7d792
azziimm7 <azzah31@outlook.com>
2024-08-01 17:37:50 +0000

    to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+A module that defines a function that
+Returns the string representation of a floor
+"""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a floor number"""
+    return str(n)
