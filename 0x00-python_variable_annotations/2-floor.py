4338a49f23dc2b381aa1ec1c8637a356dd897c09
azziimm7 <azzah31@outlook.com>
2024-08-01 17:36:51 +0000

    flor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A module that defines a function that find the floor of a number"""
+
+
+def floor(n: float) -> int:
+    """Return the floor of a number"""
+    return int(n)
