a6c29ba9dfed08e4bb451303aa352ac7686edd4f
azziimm7 <azzah31@outlook.com>
2024-08-01 17:39:07 +0000

    vars

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+A module that define variables with thier annotations
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
